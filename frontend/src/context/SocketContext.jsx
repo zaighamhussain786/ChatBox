067122226c45ea22eb33764a2f781f87df6d2770
Zaigham Hussain <zaighamhussain786@gmail.com>
2024-10-28 14:02:34 +0530

    replaced https with wss

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatbox-u3x5.onrender.com", {
+      const socket = io("wss://chatbox-u3x5.onrender.com", {
         query: {
           userId: authUser._id,
         },
